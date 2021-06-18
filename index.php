bd49baa4b0a3f830918c3d4aa415a7a2a5308e8a
pierre01470 <gonzales.pierre01470@gmail.com>
2021-06-18 15:18:50 +0200

    truc

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/vendor/altorouter/altorouter/AltoRouter.php';
+require_once __DIR__ . '/vendor/autoload.php';
+
+$router = new AltoRouter();
+$router->setBasePath('/ski_api');
+
+$router->map('GET', '/', 'ControllerCategory#allCategories');
+
+$router->map('POST', '/insertParticipants', 'ControllerParticipants#insertParticipants');
+
+$match = $router->match();
+if ($match) {
+    list($controller, $action) = explode('#', $match['target']);
+    $obj = new $controller();
+    callControl($obj, $action, $match);
+}
+function callControl($obj, $action, $match)
+{
+    if (is_callable(array($obj, $action))) {
+        call_user_func_array(array($obj, $action), array($match['params']));
+    }
+}
